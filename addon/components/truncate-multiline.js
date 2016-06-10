9e005f095cb0170ed070ab8519befa8f5c53a8e1
James Menera <menerajames@gmail.com>
2016-06-10 15:52:16 -0700

    trying in the didRender event hook instead

--- addon/components/truncate-multiline.js
+++ addon/components/truncate-multiline.js
@@ -119,7 +119,13 @@ export default Ember.Component.extend(ResizeHandlerMixin, {
    * Kicks off the truncation after render.
    * @return {Void}
    */
-  didInsertElement() {
+  // didInsertElement() {
+  //   if (!this.get('_didTruncate')) {
+  //     this._doTruncation();
+  //   }
+  // },
+  
+  didRender() {
     if (!this.get('_didTruncate')) {
       this._doTruncation();
     }
