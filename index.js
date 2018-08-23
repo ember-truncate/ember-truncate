109964228ceeb7f34652c06211bbec43a41d7614
Steve Calvert <steve.calvert@gmail.com>
2018-08-23 16:02:38 -0700

    Ensures this addon calls `_super` in `included`

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 module.exports = {
   name: 'ember-truncate',
   included: function(app) {
+    this._super.included.apply(this, arguments);
+    
     app.import('vendor/styles/truncate-multiline.css');
   }
 };
