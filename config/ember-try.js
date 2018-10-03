50f3383ab71560989f860c1cca72777b4f6e326f
Abilash Badri <abadri@linkedin.com>
2018-10-03 09:37:09 -0700

    Code review update

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,7 +9,7 @@ module.exports = function() {
     getChannelURL('beta')
   ]).then((urls) => {
     return {
-      useYarn: false,
+      useYarn: true,
       scenarios: [{
         name: 'ember-lts-2.12',
         npm: {
