9679a8063d2633e0fd766c0a01cc9e6e3abe14db
Jordan Hawker <hawker.jordan@gmail.com>
2021-06-03 11:45:26 -0700

    Fix linting config and linting errors

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 /**
  * Once a deprecation is addressed, configure its handler to `throw`.
  * Please do not remove an entry until the upgrade effort is stable.
@@ -7,8 +9,11 @@ self.deprecationWorkflow.config = {
   // If we un-comment the line below, any unhandled error i.e a new error introduced would fail the tests.
   // throwOnUnhandled: true,
   workflow: [
-    { handler: 'silence', matchId: 'deprecated-run-loop-and-computed-dot-access' },
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
     { handler: 'silence', matchId: 'ember-global' },
     { handler: 'silence', matchId: 'this-property-fallback' },
-  ]
+  ],
 };
